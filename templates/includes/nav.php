225add2985a696a9fff00d519c5aaa3966b5da8e
Maxim Svechihin <folsinap@yandex.ru>
2021-12-27 17:41:30 +0400

    beautify nav html

--- templates/includes/nav.php
+++ templates/includes/nav.php
@@ -1,21 +1,20 @@
-<ul class="nav">
-    <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="/">Home</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="/form">Form</a>
-    </li>
-    #anon()
-    <li class="nav-item">
-        <a class="nav-link" href="/register">Register</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="/login">login</a>
-    </li>
-    #endanon
-    #auth()
-    <li class="nav-item">
-        <a class="nav-link" href="/logout">logout</a>
-    </li>
-    #endauth
-</ul>
+<nav>
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        #anon()
+        <li class="nav-item">
+            <a class="nav-link" href="/register">Register</a>
+        </li>
+        <li class="nav-item float-right">
+            <a class="nav-link" href="/login">login</a>
+        </li>
+        #endanon
+        #auth()
+        <li class="nav-item">
+            <a class="nav-link" href="/logout">logout</a>
+        </li>
+        #endauth
+    </ul>
+</nav>
