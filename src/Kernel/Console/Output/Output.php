c11f6b4dd531dc859d13a3e9a8d0a061d21468d5
Maxim Svechihin <folsinap@yandex.ru>
2022-01-21 17:54:34 +0400

    add tryAgain() and choose() methods

--- src/Kernel/Console/Output/Output.php
+++ src/Kernel/Console/Output/Output.php
@@ -5,6 +5,7 @@ namespace Fwt\Framework\Kernel\Console\Output;
 class Output
 {
     protected const YES = ['yes', 'y'];
+    protected const NO = ['no', 'n'];
     protected const NEXT_LINE = "\n";
 
     public function input(string $message)
@@ -17,12 +18,48 @@ class Output
     public function confirm(string $message): bool
     {
         $answer = $this->input($message . ' [y/n] (n): ');
+        $answer = $answer === '' ? 'n' : $answer;
+        $answer = strtolower($answer);
 
-        if (in_array(strtolower($answer), self::YES)) {
+        if (in_array($answer, self::YES)) {
             return true;
+        } elseif (in_array($answer, self::NO)) {
+            return false;
         }
 
-        return false;
+        return $this->tryAgain();
+    }
+
+    /**
+     * @param string $message
+     * @param string[] $choices
+     *
+     * @return string
+     */
+    public function choose(string $message, array $choices): string
+    {
+        $choice = $this->input(
+            MessageBuilder::getBuilder()
+            ->type($message)
+            ->foreach($choices, function ($key, $value) {
+                return MessageBuilder::getBuilder()
+                    ->nextLine()->tab()
+                    ->startYellow()->write("[$key] ")->closeColor()
+                    ->startGreen()->type($value)->closeColor();
+            })
+            ->nextLine()
+            ->getMessage()
+        );
+
+        if (array_key_exists($choice, $choices)) {
+            return $choices[$choice];
+        }
+
+        if (!in_array($choice, $choices)) {
+            return $this->tryAgain();
+        }
+
+        return $choice;
     }
 
     public function success(string $message): void
@@ -88,5 +125,16 @@ class Output
     public function print(string $message): void
     {
         print $message;
+    }
+
+    protected function tryAgain()
+    {
+        $previous = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 2)[1];
+        $method = $previous['function'];
+        $args = $previous['args'];
+
+        $this->line('Try again.');
+
+        return $this->$method(...$args);
     }
 }
