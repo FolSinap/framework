6c50e01c80c96b9ded0d7840c92e2ea1e19b84cf
FolSinap <maksanya16@gmail.com>
2022-01-24 14:18:39 +0400

    upgrade make:model command

--- src/Kernel/Console/Commands/Make/MakeModelCommand.php
+++ src/Kernel/Console/Commands/Make/MakeModelCommand.php
@@ -11,6 +11,8 @@ use Fwt\Framework\Kernel\Database\ORM\Relation\AbstractRelation;
 class MakeModelCommand extends AbstractMakeCommand
 {
     protected string $namespace;
+    protected array $uses = [];
+    protected array $columns = [];
 
     public function __construct()
     {
@@ -46,6 +48,7 @@ class MakeModelCommand extends AbstractMakeCommand
 
     public function execute(Input $input, Output $output): void
     {
+        //todo: add recursive dir creation
         if (!empty($params = $input->getParameters())) {
             $name = $params[0];
         } else {
@@ -61,6 +64,7 @@ class MakeModelCommand extends AbstractMakeCommand
             'namespace' => $this->namespace,
             'relations' => $relations,
             'columns' => $columns,
+            'use' => $this->renderUses(),
         ]);
 
         if ($this->createFile("$name.php", $stub)) {
@@ -70,15 +74,36 @@ class MakeModelCommand extends AbstractMakeCommand
         }
     }
 
+    protected function renderUses(): string
+    {
+        return MessageBuilder::getBuilder()
+            ->foreach($this->uses, function ($key, $use) {
+                return "use $use;\n";
+            });
+    }
+
+    protected function addUse(string $class): void
+    {
+        if (in_array($class, $this->uses)) {
+            return;
+        }
+
+        $className = ltrim($class, $this->namespace);
+
+        if (count(explode('\\', $className)) > 1) {
+            $this->uses[] = $class;
+        }
+    }
+
     protected function renderColumns(Output $output): string
     {
-        $columns = $this->buildColumns($output);
+        $this->buildColumns($output);
 
-        if (!empty($columns)) {
+        if (!empty($this->columns)) {
             $definition = MessageBuilder::getBuilder()
                 ->nextLine()
                 ->tab()->writeln('protected static array $columns = [')
-                ->tab()->foreach($columns, function ($key, $column) {
+                ->tab()->foreach($this->columns, function ($key, $column) {
                         return "'$column',\n";
                     })
                 ->dropTab()->writeln("];");
@@ -87,19 +112,23 @@ class MakeModelCommand extends AbstractMakeCommand
         return $definition ?? '';
     }
 
-    protected function buildColumns(Output $output): array
+    protected function buildColumns(Output $output): void
     {
         if (!$output->confirm('Define columns array?')) {
-            return [];
+            $this->columns = [];
+
+            return;
         }
 
-        $columns = [];
+        $columns = empty($this->columns) ? '' : '[' . implode(', ', $this->columns) . ']';
 
-        while (($name = $output->input('Column name [Enter to stop]: ')) !== '') {
-            $columns[] = $name;
+        while (($name = $output->input("Column name $columns (Empty to stop): ")) !== '') {
+            if (!in_array($name, $this->columns)) {
+                $this->columns[] = $name;
+            }
+
+            $columns = '[' . implode(', ', $this->columns) . ']';
         }
-
-        return $columns;
     }
 
     protected function renderRelations(Output $output): string
@@ -118,8 +147,12 @@ class MakeModelCommand extends AbstractMakeCommand
                 ->tab(2)
                     ->writeln("'$name' => [")
                     ->tab()->foreach($relation, function ($key, $value) {
-                            return "'$key' => '$value',\n";
-                            })
+                            if (!in_array($key, ['type', 'class'])) {
+                                $value = "'$value'";
+                            }
+
+                            return "'$key' => $value,\n";
+                        })
                     ->dropTab()->writeln("],");
         }
 
@@ -134,21 +167,31 @@ class MakeModelCommand extends AbstractMakeCommand
         while ($output->confirm($message)) {
             $relation = [];
             $relation['name'] = $output->input('Insert relation name: ');
-            $relation['type'] = $type = $output->choose('Choose relation type', AbstractRelation::TYPES);
-            $relation['class'] = $related = "$this->namespace\\" . $output->input("Insert related class $this->namespace\\");
+            $type = $output->choose('Choose relation type', AbstractRelation::TYPES);
+            $related = "$this->namespace\\" . $output->input("Insert related class $this->namespace\\");
+            $this->addUse($related);
+            $this->addUse(AbstractRelation::class);
+
+            $className = explode('\\', $related);
+            $relation['class'] = array_pop($className) . '::class';
 
             switch ($type) {
                 case AbstractRelation::TO_ONE:
-                    $relation['field'] = $output->input("Insert field name that represents $related in DB: ");
+                    $relation['field'] = $field = $output->input("Insert field name that represents $related in DB: ");
+                    $relation['type'] = 'AbstractRelation::TO_ONE';
+                    $this->columns[] = $field;
 
                     break;
                 case AbstractRelation::ONE_TO_MANY:
                     $relation['field'] = $output->input("Insert field name in $related that represents this object in DB: ");
+                    $relation['type'] = 'AbstractRelation::ONE_TO_MANY';
 
                     break;
                 case AbstractRelation::MANY_TO_MANY:
                     $relation['field'] = $output->input("Insert field name in pivot table that represents $related in DB: ");
                     $relation['defined_by'] = $output->input("Insert field name in pivot table that represents this object in DB: ");
+                    $relation['type'] = 'AbstractRelation::MANY_TO_MANY';
+
                     $pivot = $output->input('Name of pivot table (empty for default): ');
 
                     if ($pivot !== '') {
