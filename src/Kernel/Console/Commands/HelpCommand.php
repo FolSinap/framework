2763d9e5deaa7f33b611dc3b3f7642e5455e5ecf
Maxim Svechihin <folsinap@yandex.ru>
2022-03-02 13:59:58 +0400

    upgrade command list

--- src/Kernel/Console/Commands/HelpCommand.php
+++ src/Kernel/Console/Commands/HelpCommand.php
@@ -6,14 +6,13 @@ use FW\Kernel\Console\CommandRouter;
 use FW\Kernel\Console\Input;
 use FW\Kernel\Console\Output\MessageBuilder;
 use FW\Kernel\Console\Output\Output;
+use function Symfony\Component\VarDumper\Dumper\esc;
 
 class HelpCommand extends Command
 {
-    protected CommandRouter $router;
-
-    public function __construct(CommandRouter $router)
-    {
-        $this->router = $router;
+    public function __construct(
+        protected CommandRouter $router
+    ) {
     }
 
     public function getName(): string
@@ -30,16 +29,45 @@ class HelpCommand extends Command
     {
         $map = $this->router->getMap();
 
+        $simple = [];
+        $complicated = [];
+
+        foreach ($map as $name => $command) {
+            if (str_contains($name, ':')) {
+                $prefix = array_first(explode(':', $name));
+                $complicated[$prefix][$name] = $command;
+            } else {
+                $simple[$name] = $command;
+            }
+        }
+
+        ksort($complicated);
+
         $message = MessageBuilder::getBuilder()
             ->tab()->writeln("COMMANDS:")
-            ->tab()->foreach($map, function ($key, $command) {
-                $spaces = 20 - strlen($command->getName());
+            ->tab()->foreach($simple, function (string $name, ICommand $command) {
+                $spaces = 20 - strlen($name);
 
                 return MessageBuilder::getBuilder()
-                    ->startGreen()->type($command->getName())->closeColor()
+                    ->startGreen()->type($name)->closeColor()
                     ->space($spaces)
                     ->startBlue()->type($command->getDescription())->closeColor()
                     ->nextLine();
+            })
+            ->skipLines()
+            ->foreach($complicated, function (string $prefix, array $commands) {
+                return MessageBuilder::getBuilder()
+                    ->startYellow()->write($prefix)->closeColor()
+                    ->nextLine()->tab(2)
+                    ->foreach($commands, function (string $name, ICommand $command) {
+                        $spaces = 20 - strlen($name);
+
+                        return MessageBuilder::getBuilder()
+                            ->startGreen()->type($name)->closeColor()
+                            ->space($spaces)
+                            ->startBlue()->type($command->getDescription())->closeColor()
+                            ->nextLine();
+                    })->nextLine();
             })
             ->getMessage();
 
