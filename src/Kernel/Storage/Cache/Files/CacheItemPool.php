f926e41a3330ab0119cc7cc14ca8544dc2e46576
Maxim Svechihin <folsinap@yandex.ru>
2022-03-09 19:15:32 +0400

    refactor files driver

--- src/Kernel/Storage/Cache/Files/CacheItemPool.php
+++ src/Kernel/Storage/Cache/Files/CacheItemPool.php
@@ -2,8 +2,10 @@
 
 namespace FW\Kernel\Storage\Cache\Files;
 
+use Carbon\Carbon;
 use Psr\Cache\CacheItemInterface;
 use FW\Kernel\Storage\Cache\CacheItemPool as AbstractPool;
+use FW\Kernel\Storage\Cache\CacheItem;
 
 class CacheItemPool extends AbstractPool
 {
@@ -19,7 +21,17 @@ class CacheItemPool extends AbstractPool
      */
     public function getItem(string $key): CacheItemInterface
     {
-        return new CacheItem($key);
+        $value = $this->read($key)['value'];
+
+        if (!file_exists($this->dir . '/' . $key)) {
+            return new CacheItem($key, null, false);
+        }
+
+        return new CacheItem(
+            $key,
+            $value,
+            true
+        );
     }
 
     /**
@@ -59,7 +71,11 @@ class CacheItemPool extends AbstractPool
             mkdir($this->dir, 0777, true);
         }
 
-        $content = serialize($item->getContent());
+        $item->hit();
+        $content = serialize([
+            'value' => $item->get(),
+            'expires_at' => $item->expiration(),
+        ]);
 
         return file_put_contents($this->dir . '/' . $item->getKey(), $content) !== false;
     }
@@ -76,5 +92,33 @@ class CacheItemPool extends AbstractPool
         }
 
         return $success;
+    }
+
+    protected function read(string $file): array
+    {
+        $file = $this->dir . '/' . $file;
+
+        if (file_exists($file)) {
+            $content = unserialize(file_get_contents($file));
+
+            if ($this->isExpired($content['expires_at'])) {
+                unlink($file);
+
+                $content = ['value' => null, 'expires_at' => null];
+            }
+        } else {
+            $content = ['value' => null, 'expires_at' => null];
+        }
+
+        return $content;
+    }
+
+    protected function isExpired(string $expiration): bool
+    {
+        if (is_null($expiration)) {
+            return false;
+        }
+
+        return $expiration < Carbon::now();
     }
 }
